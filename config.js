e43712db31a0242387466c0773e20cb66fd39699
modalqa <49194243+modalqa@users.noreply.github.com>
2019-04-02 18:37:25 +0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-export default {
-    baseUrl: "https://www.modalrakyat.id/",
-    devUrl: "https://develop-p2p.modalrakyat.id/login",
-    qaUrl: "https://testing-p2p.modalrakyat.id/login"
-    
-};
